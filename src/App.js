abd81b70a976b7c19bb17dd275f259ee2c1840ab
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-06-30 12:15:23 -0400

    Fetch data from Github via github api and create a Spinner React Component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import './App.css';
+import Axios from 'axios';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await Axios.get('https://api.github.com/users');
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <div className='App'>
         <Navbar title='Github Finder' icon='fab fa-github'></Navbar>
         <div className='container'>
-          <Users></Users>
+          <Users loading={this.state.loading} users={this.state.users}></Users>
         </div>
       </div>
     );
