47d1ba6340b38663fc0f1e5ecd4465cfa4873bc2
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-07-06 13:37:52 -0400

    Create alert Context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Alert from './components/layout/Alert';
@@ -8,40 +8,35 @@ import User from './components/users/User';
 import About from './components/pages/About';
 import './App.css';
 import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
 
 const App = () => {
-  const [alert, setAlert] = useState(null);
-
-  // Set an alert when the search field is blank
-  const showAlert = (msg, type) => {
-    setAlert({ msg, type });
-    setTimeout(() => setAlert(null), 2000);
-  };
-
   return (
     <GithubState>
-      <Router>
-        <div className='App'>
-          <Navbar title='Github Finder' icon='fab fa-github'></Navbar>
-          <div className='container'>
-            <Alert alert={alert}></Alert>
-            <Switch>
-              <Route
-                exact
-                path='/'
-                render={(props) => (
-                  <Fragment>
-                    <Search setAlert={showAlert}></Search>
-                    <Users></Users>
-                  </Fragment>
-                )}
-              ></Route>
-              <Route exact path='/about' component={About}></Route>
-              <Route exact path='/user/:login' component={User}></Route>
-            </Switch>
+      <AlertState>
+        <Router>
+          <div className='App'>
+            <Navbar title='Github Finder' icon='fab fa-github'></Navbar>
+            <div className='container'>
+              <Alert></Alert>
+              <Switch>
+                <Route
+                  exact
+                  path='/'
+                  render={(props) => (
+                    <Fragment>
+                      <Search></Search>
+                      <Users></Users>
+                    </Fragment>
+                  )}
+                ></Route>
+                <Route exact path='/about' component={About}></Route>
+                <Route exact path='/user/:login' component={User}></Route>
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </AlertState>
     </GithubState>
   );
 };
--- src/components/layout/Alert.js
+++ src/components/layout/Alert.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useContext } from 'react';
+import AlertContext from '../../context/alert/alertContext';
 
-const Alert = ({ alert }) => {
+const Alert = () => {
+  const alertContext = useContext(AlertContext);
+
+  const { alert } = alertContext;
   return (
     alert !== null && (
       <div className={` alert alert-${alert.type}`}>
@@ -9,10 +12,6 @@ const Alert = ({ alert }) => {
       </div>
     )
   );
-};
-
-Alert.propsTypes = {
-  alert: PropTypes.object.isRequired,
 };
 
 export default Alert;
